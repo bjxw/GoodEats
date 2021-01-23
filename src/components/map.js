749c1480470abadf501a03e59ff881d03383a8d0
Ben Wu <benjxwu@stanford.edu>
2021-01-22 16:18:18 -0800

    Placing basic markers on map. Fumbled with google-map-react vs google-maps-react

--- src/components/map.js
+++ src/components/map.js
@@ -1,46 +1,55 @@
 import React, {Component} from 'react'
-import GoogleMapReact from 'google-map-react' //Google map package
-import Marker from './marker'; //Marker component
-require('dotenv').config()
-
+import GoogleMapReact from 'google-map-react'; //see https://github.com/google-map-react/google-map-react/blob/master/API.md
+import Marker from './marker'
 
 class GoogleMap extends Component {
     constructor(props){
         super(props);
-        this.markerClick = this.markerClick.bind(this);
-    }
-
-    markerClick(position) {
-        var markerPos = {
-            lat: position.lat,
-            lng: position.lng
+        this.addMarker = this.addMarker.bind(this);
+        this.state = {
+            markers: [
+                {lat: 34.08421909476845, lng: -118.07298836096781}, // in-n-out
+                {lat: 34.07993604059942, lng: -118.08234390563354}, // bay island
+                {lat: 34.07583050324687, lng: -118.07335314159903}, // bodhi veggie cuisine
+                {lat: 34.10543567839181, lng: -118.07300981856079}, // green zone
+                {lat: 34.090687695735866, lng: -118.0529254379374}, // popeyes
+            ]
         }
-        console.log(position);
-        console.log(markerPos);
     }
 
-    static defaultProps = {
-        center: {
-            lat: 34.08,
-            lng: -118.07
-        },
-        zoom: 12
+    addMarker(e){
+        //console.log(e); //OUTPUTS: ({ x, y, lat, lng, event })
     }
 
     render(){
-        return(
-            <div style={{height: '90vh', width: '90%'}}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{key: process.env.GOOGLE_MAPS_API_KEY}}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                    onClick={this.markerClick}
-                >
+        const defaultCenter = {
+            lat: 34.08070866379608,
+            lng: -118.0738933692873
+        }
 
+        var markers = this.state;
+        console.log(markers);
+
+        return(
+            <div style={{height: '95vh', width: '90vw'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: 'hidden'}}
+                    defaultCenter={defaultCenter}
+                    defaultZoom={12}
+                    onClick={this.addMarker}
+                >
+                    {this.state.markers.map((marker) =>(
+                        <Marker
+                            lat={marker.lat}
+                            lng={marker.lng}
+                        />
+                    ))}
                 </GoogleMapReact>
             </div>
+            
         );
     }
 };
 
 export default GoogleMap;
+
