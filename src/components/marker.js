8529b7ef7d9789a4301becb599a7fbbde863b622
Ben Wu <ben.jx.wu@gmail.com>
2021-01-22 16:18:18 -0800

    Placing basic markers on map. Fumbled with google-map-react vs google-maps-react

--- src/components/marker.js
+++ src/components/marker.js
@@ -1,2 +1,22 @@
-import {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react'
+
+const Marker = ({show, place}) => {
+    const markerStyle = {
+        border: '1px solid white',
+        borderRadius: '50%',
+        height: 10,
+        width: 10,
+        backgroundColor: show ? 'red' : 'blue',
+        cursor: 'pointer',
+        zIndex: 10,
+    }
+
+    return(
+        <div>
+            <div style={markerStyle}/>
+            {/* {show && <InfoWindow place={place}/>} */}
+        </div>
+    )
+}
+
+export default Marker;
