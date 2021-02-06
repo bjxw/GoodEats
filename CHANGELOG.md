46937772494d8c163c83dfd5feff2a36c557c216
Ben Wu <benjxwu@stanford.edu>
2021-02-05 16:32:44 -0800

    Added changelog for updates. Cleaning up UI/CSS files and icons.

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 2/5/2021
+- Added npm package bootstrap
+- Added App.js CSS
+- Added favico
--- src/components/markerWindow.js
+++ src/components/markerWindow.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+
 import "./css/markerWindow.css"
 
 const infoWindowStyle = {
